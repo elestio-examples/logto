c4d28a0041fd44cb9420aba6bb7602499e562b9c
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-12 13:52:25 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,3 +6,13 @@ echo "Waiting for software to be ready ..."
 sleep 30s;
 
 docker-compose exec -T app sh -c "npm run cli db seed"
+
+if [ -e "./initialized" ]; then
+    echo "Already initialized, skipping..."
+else
+    # If the file doesn't exist, create it and echo "popopo"
+    docker-compose exec -T app sh -c "npm run cli db seed"
+    docker-compose exec -T app sh -c "npm run alteration deploy latest"
+    sed -i "s~# ~~g" ./docker-compose.yml
+    touch "./initialized"
+fi
