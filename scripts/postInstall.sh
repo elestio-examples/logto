00f7cc6596eed7441192c5782bd374c8ac9dcbaf
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-13 15:17:05 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,8 +8,8 @@ sleep 30s;
 if [ -e "./initialized" ]; then
     echo "Already initialized, skipping..."
 else
-    docker-compose exec -T app sh -c "npm run cli db seed"
     docker-compose exec -T app sh -c "npm run alteration deploy latest"
+    sed -i "s~npm run cli db seed && ~~g" ./docker-compose.yml
     touch "./initialized"
 fi
 
